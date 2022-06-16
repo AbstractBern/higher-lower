d8fc15bc5fbc39f8a9e39914075262058247c27f
bernard flores <43194537+AbstractBern@users.noreply.github.com>
2022-06-16 15:21:18 -0500

    Update higher-lower.js
    There are easier ways to validate. I shortened 35~ lines

--- scripts/higher-lower.js
+++ scripts/higher-lower.js
@@ -5,21 +5,22 @@ let randomNumber = Math.floor( Math.random() * max) + 1;
 function Guess() {
     let textInput = document.getElementById('guess').value;
     let display = document.getElementById('prompt');
-    var result = parseInt(textInput);
     console.log(randomNumber);
-    if ( isDigit(textInput) === -1) {
-        display.style.color = "red";
-        display.innerText = "That number is not in range, try again.";
-    } else if ( !isDigit(textInput)) {
+    var result = Number(textInput);
+
+    if ( Number.isInteger(Number(textInput)) === false ) {
+        console.log("Rounding");
+        result = Math.round(result);
+        console.log(result);
+    }
+    if ( isNaN(Number(textInput)) ) {
+        console.log("HEY");
         display.style.color = "red";
         display.innerText = "That is not a number!";
-    } else if ( result < 1 || (result > max ) ) {
+    } else if ( ( Number.isInteger(Number(textInput)) === true || Number.isInteger(Number(textInput)) === false )  && ( result < 1 || result > max ) ) {
         display.style.color = "red";
         display.innerText = "That number is not in range, try again.";
     } else {
-        if (isDigit(textInput) === 2) {
-            result = Math.round(textInput);
-        }
         if ( guesses.includes(result)) {
             display.style.color = "red";
             display.innerText = "This guess has been attempted and will not be counted!";
@@ -44,7 +45,7 @@ function Guess() {
                         display.innerText += ", "
                     }
                 }
-                display.innerText += "\n**A new number to guess has been generated.**\n**Refreshing tracked guesses.";
+                display.innerText += "\n**A new number to guess has been generated.**\n**Refreshing tracked guesses.**";
                 guesses.splice(0, guesses.length);
                 randomNumber = Math.floor( Math.random() * max) + 1;
             }
@@ -71,30 +72,4 @@ function setMaximumRange() {
     let prompt = document.getElementById('range-prompt');
     prompt.innerText = `Guess a number between 1 and ${max}.`;
     randomNumber = Math.floor( Math.random() * max) + 1;
-}
-
-function isDigit(input) {
-    let isDig = 1;
-    let isNeg = 0;
-    let needsRounded = 0;
-    if (input.charAt(0) === '-' ) {
-        isNeg = 1;
-    }
-    for(let i = 0; i < input.length; i++) {
-        if (isNaN(input[i]) && input[i] != '.' && input[i] != '-') {
-            isDig = 0;
-            break;
-        } else if (input[i] === '.' ) {
-            needsRounded = 1;
-            continue;
-        } else if ( input[i] === '-') {
-            continue;
-        }
-    }
-    if (isNeg && isDig) {
-        return -1;
-    } else if (isDig && needsRounded) {
-        return 2;
-    }
-    return isDig;
 }
